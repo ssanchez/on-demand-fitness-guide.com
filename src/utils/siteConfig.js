d86be45bdabc9f50b2b167366611413867e1523e
Spencer Sanchez <dev@spencersanchez.com>
2020-01-10 21:54:41 -0500

    Set site config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,21 +1,20 @@
 module.exports = {
-  siteTitle: 'GCN',
-  siteTitleAlt: 'GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
-  publisher: 'Publisher named GCN', // Organization name used for SEO schema
-  siteDescription:
-    'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
-  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
-  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
-  postsPerPage: 6, // Number of posts shown on paginated pages
-  author: 'GCN User', // Author for RSS author segment and SEO schema
-  authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
-  userTwitter: '@twitter', // Change for Twitter Cards
-  shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
-  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
-  shareImageWidth: 900, // Change to the width of your default share image
-  shareImageHeight: 600, // Change to the height of your default share image
-  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
-  backgroundColor: '#e9e9e9', // Used for Offline Manifest
-  themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+	siteTitle: 'On-Demand Fitness Guide',
+	siteTitleAlt: 'On-Demand Fitness Guide', // This allows an alternative site title for SEO schema.
+	publisher: 'On-Demand Fitness Guide', // Organization name used for SEO schema
+	siteDescription: 'Connecting you to your connected home fitness',
+	siteUrl: 'https://www.ondemandfitnessguide.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+	postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+	postsPerPage: 6, // Number of posts shown on paginated pages
+	author: 'Staff Writer', // Author for RSS author segment and SEO schema
+	authorUrl: 'https://www.ondemandfitnessguide.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+	userTwitter: '@ondemandfitnessguide', // Change for Twitter Cards
+	shortTitle: 'ODFG', // Used for App manifest e.g. Mobile Home Screen
+	shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+	shareImageWidth: 900, // Change to the width of your default share image
+	shareImageHeight: 600, // Change to the height of your default share image
+	siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+	backgroundColor: '#e9e9e9', // Used for Offline Manifest
+	themeColor: '#121212', // Used for Offline Manifest
+	copyright: 'Copyright © 2020 On-Demand Fitness Guide', // Copyright string for the RSS feed
 }
